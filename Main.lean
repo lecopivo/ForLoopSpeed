0a29c7f9ffef31cdbd085be1af3f13aa8fd64507
lecopivo <skrivantomas@seznam.cz>
2023-04-20 07:32:31 +0200

    changed to more complicated computation that does not get optimized away

--- Main.lean
+++ Main.lean
@@ -1,35 +1,37 @@
 import ForLoopSpeed
 
+instance : Coe USize Float := ⟨λ n => n.toUInt64.toFloat⟩
 
-def triangularNumberNaive (n : USize) : IO USize := pure $ Id.run do
-  let mut sum : USize := 0
+def triangularNumberNaive (n : USize) : IO Float := pure $ Id.run do
+  let mut sum : Float := 0
   for i in [0:n.toNat] do
-    for _ in [0:i] do
-      sum := sum + 1
+    for j in [0:i] do
+      sum := sum + Float.sin i.toFloat + Float.cos j.toFloat
   pure sum
 
-def triangularNumberForIn (n : USize) : IO USize := pure $ Id.run do
-  let mut sum : USize := 0
+
+def triangularNumberForIn (n : USize) : IO Float := pure $ Id.run do
+  let mut sum : Float := 0
   for i in (fullRange n) do
-    for _ in fullRange i.1 do
-      sum := sum + 1
+    for j in fullRange i.1 do
+      sum := sum + Float.sin i.1 + Float.cos j.1
   pure sum
 
-def triangularNumberFoldM (n : USize) : IO USize := pure $ Id.run do
+def triangularNumberFoldM (n : USize) : IO Float := pure $ Id.run do
   Idx.foldM (fullRange n) (init := 0) λ sum i => 
-    Idx.foldM (fullRange i.1) (init := sum) λ sum' i => sum'+1
+    Idx.foldM (fullRange i.1) (init := sum) λ sum' j => sum'+Float.sin i.1 + Float.cos j.1
 
-def triangularNumberFold (n : USize) : IO USize := pure $ 
+def triangularNumberFold (n : USize) : IO Float := pure $ 
   Idx.fold (fullRange n) (init := 0) λ sum i => 
-    Idx.fold (fullRange i.1) (init := sum) λ sum' i => sum'+1
+    Idx.fold (fullRange i.1) (init := sum) λ sum' j => sum'+Float.sin i.1 + Float.cos j.1
 
-def testFun (f : USize → IO USize) (n : USize) (msg : String) : IO Unit := do
+def testFun (f : USize → IO Float) (n : USize) (msg : String) : IO Unit := do
   let (m, t) ← time (f n)
-  IO.println s!"Computed {n}-th triangular number {m} with {msg} in: time {t.toFloat/1000000.0}ms"
+  IO.println s!"Computing for n := {n}, result := {m}, method := {msg}, time := {t.toFloat/1000000.0}ms"
 
 def main (args : List String) : IO Unit := do
   
-  let n := (args.head!.toNat?.getD 1000000).toUSize
+  let n := (args.head!.toNat?.getD 1000).toUSize
 
   -- testFun triangularNumberNaive n "naive for loop"
   testFun triangularNumberForIn n "for loop"
--- main.cpp
+++ main.cpp
@@ -1,30 +1,30 @@
 #include <iostream>
 #include <chrono>
 #include <sstream>
-
+#include <cmath>
 
 int main (int argc, char *argv[]) {
 
-    int n = 1000000;
+    int n = 1000;
     if (argc > 1) {
         std::stringstream ss(argv[1]);
         ss >> n;
     }
 
 
-    long long sum = 0;
+    float sum = 0;
 
     auto start = std::chrono::steady_clock::now();
 
     for (int i=0; i<n; i++){
         for (int j=0; j<i; j++){
-            sum += 1;
+            sum += sin(i) + cos(j);
         }
     }
  
     auto end = std::chrono::steady_clock::now();
 
-    std::cout << "Computed " << n << "-th triangular number " << sum << " in C++ in: "
+    std::cout << "Computing for n := " << n << ", result := " << sum << ", method := c++ for loop, time := "
               << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count()
               << "ms" << std::endl;
 
